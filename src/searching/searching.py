e5fc5f57953483750a9bc57255ef7e458028a26b
Sable <sablechilds@yahoo.com>
2020-10-06 20:44:44 -0400

    searching-fail

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,27 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    if start > end:
+        return 1
+    midpoint = (start + end) // 2
+
+    if target == arr[midpoint]:
+        return midpoint
+    
+    else:
+        if target < arr[midpoint]:
+
+            end = midpoint - 1
+        else: start = midpoint + 1
+        
+        
+        return binary_search(arr, target, start, end)
+
+    
+    # starting point
+    # ending point
+    # target value. Check list of target value and if found return it
+    # if not found, keep going,
+    # base case = target found
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,6 +30,6 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
+#def agnostic_binary_search(arr, target):
     # Your code here
 
